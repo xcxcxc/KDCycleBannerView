aaa34142022905eb345c181c680b699a92b495a1
xc <xc@veryxc.com>
2015-09-02 14:43:28 +0800

    修服 会卡在中间

--- KDCycleBannerView/KDCycleBannerView.m
+++ KDCycleBannerView/KDCycleBannerView.m
@@ -177,6 +177,10 @@ static void *kContentImageViewObservationContext = &kContentImageViewObservation
 
 - (void)moveToTargetPosition:(CGFloat)targetX withAnimated:(BOOL)animated {
     [_scrollView setContentOffset:CGPointMake(targetX, 0) animated:animated];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+		[_scrollView setContentOffset:CGPointMake(targetX, 0) animated:NO];
+	});
 }
 
 - (void)setCurrentPage:(NSInteger)currentPage animated:(BOOL)animated {
